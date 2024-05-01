4d2842804d26531db52e812163a84af31ef6f705
Vlad Bocharov <vladislavboc11@gmail.com>
2024-05-01 18:40:38 +0300

    Added TaskTimeTracker repository

---
+++ backend/Taskify/Taskify.DAL/Helpers/TaskTimeTrackerFilterBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taskify.Core.DbModels;
+
+namespace Taskify.DAL.Helpers
+{
+    public class TaskTimeTrackerFilterBuilder : BaseFilterBuilder<TaskTimeTracker>
+    {
+        public bool IncludeUser { get; private set; }
+        public bool IncludeCustomTask { get; private set; }
+
+        public TaskTimeTrackerFilterBuilder IncludeUserEntity()
+        {
+            IncludeUser = true;
+            return this;
+        }
+
+        public TaskTimeTrackerFilterBuilder IncludeCustomTaskEntity()
+        {
+            IncludeCustomTask = true;
+            return this;
+        }
+    }
+}
